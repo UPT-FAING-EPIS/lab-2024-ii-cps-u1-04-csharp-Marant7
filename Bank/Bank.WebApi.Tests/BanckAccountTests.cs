af346eb98bb3f0f3a33b3fdbe5c35792a40edfaf
marant7 <mf2018000597@virtual.upt.pe>
2024-09-21 01:49:56 -0500

    terminando7

--- Bank/Bank.WebApi.Tests/BanckAccountTests.cs
+++ Bank/Bank.WebApi.Tests/BanckAccountTests.cs
@@ -19,5 +19,33 @@ namespace Bank.Domain.Tests
             double actual = account.Balance;
             Assert.That(actual, Is.EqualTo(expected).Within(0.001), "Account not debited correctly");
         }
+        [Test]
+public void Credit_WithValidAmount_UpdatesBalance()
+{
+    // Arrange
+    double beginningBalance = 11.99;
+    double creditAmount = 5.01;
+    double expected = 17.00;
+    BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+    // Act
+    account.Credit(creditAmount);
+
+    // Assert
+    double actual = account.Balance;
+    Assert.That(actual, Is.EqualTo(expected).Within(0.001), "Account not credited correctly");
+}
+
+[Test]
+public void Credit_WithNegativeAmount_ShouldThrowArgumentOutOfRange()
+{
+    // Arrange
+    double beginningBalance = 11.99;
+    double creditAmount = -1.00;
+    BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+    // Act & Assert
+    Assert.Throws<ArgumentOutOfRangeException>(() => account.Credit(creditAmount));
+}
     }
 }
