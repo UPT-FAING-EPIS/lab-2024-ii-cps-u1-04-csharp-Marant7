0bc295013796eddbf4fc7776df00de099aa45e1f
marant7 <mf2018000597@virtual.upt.pe>
2024-09-21 01:22:26 -0500

    terminando5

--- Bank/Bank.WebApi/Models/BankAccount.cs
+++ Bank/Bank.WebApi/Models/BankAccount.cs
@@ -1,9 +1,6 @@
 namespace Bank.WebApi.Models
 {
-     public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-    {
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-    }
+    
     public class BankAccount
     {
         private readonly string m_customerName;
