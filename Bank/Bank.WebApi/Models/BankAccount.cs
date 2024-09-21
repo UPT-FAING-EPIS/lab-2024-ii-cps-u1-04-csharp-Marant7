5f4b070461f8f5a892c57e817bb90c8de823fa38
marant7 <mf2018000597@virtual.upt.pe>
2024-09-21 01:15:27 -0500

    terminando4

--- Bank/Bank.WebApi/Models/BankAccount.cs
+++ Bank/Bank.WebApi/Models/BankAccount.cs
@@ -1,41 +1,33 @@
 namespace Bank.WebApi.Models
 {
+     public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
     public class BankAccount
     {
-       private readonly string m_customerName;
+        private readonly string m_customerName;
         private double m_balance;
-
+        
         public BankAccount(string customerName, double balance)
         {
-            if (string.IsNullOrWhiteSpace(customerName))
-                throw new ArgumentException("Customer name cannot be null or empty.", nameof(customerName));
-
-            if (balance < 0)
-                throw new ArgumentOutOfRangeException(nameof(balance), "Initial balance cannot be negative.");
-
             m_customerName = customerName;
             m_balance = balance;
         }
-
-        public string CustomerName => m_customerName;
-        public double Balance => m_balance;
-
+        public string CustomerName { get { return m_customerName; } }
+        public double Balance { get { return m_balance; }  }
         public void Debit(double amount)
         {
             if (amount > m_balance)
-                throw new ArgumentOutOfRangeException(nameof(amount), "Debit amount exceeds balance.");
-            
+                throw new ArgumentOutOfRangeException("amount");
             if (amount < 0)
-                throw new ArgumentOutOfRangeException(nameof(amount), "Debit amount cannot be negative.");
-            
+                throw new ArgumentOutOfRangeException("amount");
             m_balance -= amount;
         }
-
         public void Credit(double amount)
         {
             if (amount < 0)
-                throw new ArgumentOutOfRangeException(nameof(amount), "Credit amount cannot be negative.");
-            
+                throw new ArgumentOutOfRangeException("amount");
             m_balance += amount;
         }
     }
